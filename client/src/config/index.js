a79d2a796da36248d849414ed6d48ab9e8078366
mohaimenmahi <mmc.dp11@gmail.com>
2021-09-26 00:10:38 +0600

    CEC - Assignment -Base Url changed

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   BASE_URL:
-    process.env.NODE_ENV !== "production" && typeof window === "object"
+    process.env.NODE_ENV === "production" && typeof window === "object"
       ? "https://cec-fwd-server.herokuapp.com/"
       : "http://localhost:5001/",
 };
